d8a663918684617ddef07f644ec11fcb7866f303
Dave P <davep1405@gmail.com>
2025-08-24 21:44:46 +0700

    feat(service/Admin): Added create admin

---
+++ fast-api/app/services/admin_service.py
@@ -0,0 +1,25 @@
+
+from fastapi import HTTPException, status
+
+from ..models import Admin
+from ..schemas import AdminCreate
+from . import security_service
+
+async def create_admin(admin_data: AdminCreate):
+    existing_admin = await Admin.find_one()
+    
+    if existing_admin:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Admin already exists"
+        )
+    
+    hash_password = Admin.hash_password(admin_data.password)
+    
+    new_admin = Admin(
+        hashed_password=hash_password
+    )
+    
+    await new_admin.insert()
+    
+    return {"message": "Admin created successfully"}
