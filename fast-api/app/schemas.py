f59837a78f815462d5e91b23e4b303b891238047
Dave P <davep1405@gmail.com>
2025-08-24 21:11:09 +0700

    init(schemas): Intialize base schemas

---
+++ fast-api/app/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class AdminCreate(BaseModel):
+    hashed_password: str
+
+class NetworkDeviceCreate(BaseModel):
+    ip_address: str
+    hostname: Optional[str]
+    model: Optional[str] # e.g., 'Cisco 2901'
+    os_version: Optional[str] # e.g., 'IOS 15.2', 
+    location: Optional[str] # e.g., 'Data Center 1', 'Office 2'
