1dab06e8b07dbe6b75f2084f629950c1a22c871c
Dave P <davep1405@gmail.com>
2025-08-24 21:26:47 +0700

    fix(model/Admin): Should inherited Document instead of BaseModel

--- fast-api/app/models/Admin.py
+++ fast-api/app/models/Admin.py
@@ -1,8 +1,11 @@
-from pydantic import BaseModel
+from beanie import Document
+from passlib.context import CryptContext
 
 from . import DOCUMENT_MODELS
 
-class Admin(BaseModel):
-    password: str
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Admin(Document):
+    hashed_password: str
 
 DOCUMENT_MODELS.append(Admin)
