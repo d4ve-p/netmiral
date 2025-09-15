7a7202d7ed6e70b0b1510469c38e3a92ea08734e
Dave P <65463983+d4ve-p@users.noreply.github.com>
2025-09-15 18:27:56 +0700

    fix(middlware): Fixed middleware to be compatible with _next path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,30 @@
+// src/middleware.ts
+
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 const AUTH_COOKIE_NAME = 'auth_token';
 
-const PUBLIC_PATHS = [
-    "/auth",
-]
-
-export default function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
     const token = request.cookies.get(AUTH_COOKIE_NAME)?.value;
     const { pathname } = request.nextUrl;
 
-    const isProtectedRoute = !PUBLIC_PATHS.some(path => pathname.startsWith(path));
-
-    if (isProtectedRoute && !token) {
+    if (!token && pathname !== '/auth') {
         const loginUrl = new URL('/auth', request.url);
-        loginUrl.searchParams.set('from', request.nextUrl.pathname);
-
+        loginUrl.searchParams.set('from', pathname);
         return NextResponse.redirect(loginUrl);
     }
 
-    if (token && pathname.startsWith('/auth')) {
-        // TODO: Change to dashboard URL
-        const dashboardUrl = new URL('/', request.url);
-
+    if (token && pathname === '/auth') {
+        const dashboardUrl = new URL('/dashboard', request.url);
         return NextResponse.redirect(dashboardUrl);
     }
 
     return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
