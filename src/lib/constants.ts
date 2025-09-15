d79d7851f4e30f0b6f196c8708803707d5e485a2
Dave P <65463983+d4ve-p@users.noreply.github.com>
2025-09-15 18:27:33 +0700

    constants: changed default constant for api prefix

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 const constants = {
     BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:8000',
-    API_PREFIX: process.env.API_PREFIX || '/api'
+    API_PREFIX: process.env.API_PREFIX || ''
 }
 
 export default constants
