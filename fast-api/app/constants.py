90e00046d212c902b8b5d9f9bcd8d65e5387c4d8
Dave P <65463983+d4ve-p@users.noreply.github.com>
2025-09-16 08:12:28 +0700

    backend: added constants

---
+++ fast-api/app/constants.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY", "default key")
+SECRET_KEY_ALGORITHM= os.getenv("SECRET_KEY_ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES= os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15)
