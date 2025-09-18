59bfe4bba4e1567e7d3ad15fad562a6ee65be67b
Dave P <65463983+d4ve-p@users.noreply.github.com>
2025-09-18 14:18:54 +0700

    schema: added more network device schema

--- fast-api/app/schemas.py
+++ fast-api/app/schemas.py
@@ -1,15 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+
+from datetime import datetime
+
+from .models.NetworkDevice import DeviceStatus, DeviceCredentials, DeviceType
+
+class NetworkDeviceBase(BaseModel):
+    hostname: Optional[str]
+    location: Optional[str] # e.g., 'Data Center 1', 'Office 2'
+    device_type: DeviceType
+    
+    ip_address: Optional[str] = None # Required if active
+    status: Optional[DeviceStatus] = 'unknown' # required if active
+    
+    # --- Optional Metadata Fields ---
+    model: Optional[str] = None     
+    os_version: Optional[str] = None
 
 class CreateAdmin(BaseModel):
     password: str
 
-class CreateNetworkDevice(BaseModel):
-    ip_address: str
-    hostname: Optional[str]
-    model: Optional[str] # e.g., 'Cisco 2901'
-    os_version: Optional[str] # e.g., 'IOS 15.2', 
-    location: Optional[str] # e.g., 'Data Center 1', 'Office 2'
+class CreateNetworkDevice(NetworkDeviceBase):
+    credentials: Optional[DeviceCredentials] = None
+
+class ShowNetworkDevice(NetworkDeviceBase):
+    id: str = Field(..., alias="_id")
+    created_at: datetime
+    config_text: str
 
 class ValidateAdmin(BaseModel):
     password: str
