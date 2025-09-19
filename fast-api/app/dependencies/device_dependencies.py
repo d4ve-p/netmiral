79ff53089f44adfaf94e0d1f230cf66afe19ae6f
Dave P <65463983+d4ve-p@users.noreply.github.com>
2025-09-19 10:42:59 +0700

    dependencies(BE-device): removed unsued

--- fast-api/app/dependencies/device_dependencies.py
+++ fast-api/app/dependencies/device_dependencies.py
@@ -25,38 +25,3 @@ class CreateLocalDeviceForm:
             os_version=self.os_version,
         )
 
-class CreateActiveDeviceForm(CreateLocalDeviceForm):
-    def __init__(
-        self,
-        hostname: str = Form(...),
-        location: Optional[str] = Form(None),
-        model: str = Form(None),
-        os_version: str = Form(None),
-
-        ip_address: str = Form(...),
-        status: DeviceStatus = Form(...),
-
-        cred_username: str = Form(...),
-        cred_password: str = Form(...),
-        cred_enable_password: Optional[str] = Form(None)
-    ):
-        super(hostname, location, model, os_version)
-        
-        self.ip_address = ip_address
-        self.status = status
-
-        self.credentials = device.DeviceCredentials(
-            username=cred_username,
-            password=cred_password,
-            enable_password=cred_enable_password
-        )
-
-        return device.CreateActiveNetworkDevice(
-            hostname=self.hostname,
-            location=self.location,
-            model=self.model,
-            os_version=self.os_version,
-            ip_address=self.ip_address,
-            status=self.status,
-            credentials=self.credentials,
-        )
